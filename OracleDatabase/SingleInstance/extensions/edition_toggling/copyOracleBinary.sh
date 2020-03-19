f7e8e899999f314c767625a3ebd534d4050c907d
Rishabh Gupta <rishabh.y.gupta@oracle.com>
2020-03-19 21:36:05 +0530

    removed modified runOracle.sh and introduced a new script copyOracleBinary.sh which will support toggling of edition passed during runtime
    Signed-off-by: Rishabh Gupta <rishabh.y.gupta@oracle.com>

---
+++ OracleDatabase/SingleInstance/extensions/edition_toggling/copyOracleBinary.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# LICENSE UPL 1.0
+#
+# Copyright (c) 1982-2020 Oracle and/or its affiliates. All rights reserved.
+# 
+# Since: March, 2020
+# Author: rishabh.y.gupta@oracle.com
+# Description: Copies the correct oracle binary in accordance with the edition passed by the user.
+# 
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+# 
+
+# Default for ORACLE_EDITION
+export ORACLE_EDITION=${ORACLE_EDITION:-enterprise}
+
+if [ "${ORACLE_EDITION,,}" == "enterprise" ]; then
+  cp $ORACLE_HOME/bin/oracle_ent $ORACLE_HOME/bin/oracle
+fi
+
+if [ "${ORACLE_EDITION,,}" == "standard" ]; then
+  cp $ORACLE_HOME/bin/oracle_std $ORACLE_HOME/bin/oracle
+fi
+
+# Deleting rest of the oracle binaries
+rm $ORACLE_HOME/bin/oracle_*

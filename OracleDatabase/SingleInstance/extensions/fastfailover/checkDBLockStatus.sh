f1665c1f7e93436ad647b273ee5ddc6942b818ef
Mohammed Yunus <mohammed.qureshi@oracle.com>
2020-03-23 23:09:34 +0530

    Fix checkDBLockStatus.sh
    Signed-off-by: Mohammed Yunus <mohammed.qureshi@oracle.com>

--- OracleDatabase/SingleInstance/extensions/fastfailover/checkDBLockStatus.sh
+++ OracleDatabase/SingleInstance/extensions/fastfailover/checkDBLockStatus.sh
@@ -12,14 +12,17 @@
 ORACLE_SID="`grep $ORACLE_HOME /etc/oratab | cut -d: -f1`"
 
 "$ORACLE_BASE/$LOCKING_SCRIPT" --check --file "$ORACLE_BASE/oradata/${ORACLE_SID}.create_lck"
-if [ $? -ne 0 ]; then  # Ok create lock not held, now check status
+if [ $? -eq 0 ]; then
+  exit 1  # create lock held, DB is still initializing
+else
+  # Ok create lock not held, now check status
   "$ORACLE_BASE/$CHECK_DB_FILE"
-  if [ $? -ne 0 ]; then # DB status is not good
+  if [ $? -ne 0 ]; then
+    # DB status is not good
     "$ORACLE_BASE/$LOCKING_SCRIPT" --check --file "$ORACLE_BASE/oradata/${ORACLE_SID}.exist_lck"
-    if [ $? -eq 0 ]; then # if exist lock is held release it
+    if [ $? -eq 0 ]; then
+      # exist lock is held release it
       "$ORACLE_BASE/$LOCKING_SCRIPT" --release --file "$ORACLE_BASE/oradata/${ORACLE_SID}.exist_lck"
-    else
-      exit 1
     fi
   fi
 fi

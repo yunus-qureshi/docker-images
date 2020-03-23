d434bf55f28b739bc3b6d7aba056e964fe0ea189
Mohammed Yunus <mohammed.qureshi@oracle.com>
2020-03-23 20:35:32 +0530

    Fix status issues

--- OracleDatabase/SingleInstance/extensions/fastfailover/checkDBLockStatus.sh
+++ OracleDatabase/SingleInstance/extensions/fastfailover/checkDBLockStatus.sh
@@ -3,7 +3,7 @@
 #
 # Copyright (c) 1982-2020 Oracle and/or its affiliates. All rights reserved.
 #
-# Since: May, 2020
+# Since: Mar, 2020
 # Author: mohammed.qureshi@oracle.com
 # Description: Checks the status of Oracle Database and Locks
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
@@ -11,15 +11,15 @@
 
 ORACLE_SID="`grep $ORACLE_HOME /etc/oratab | cut -d: -f1`"
 
-"$ORACLE_BASE/$CHECK_DB_FILE"
-if [ $? -ne 0 ]; then
-  "$ORACLE_BASE/$LOCKING_SCRIPT" --check --file "$ORACLE_BASE/oradata/${ORACLE_SID}.exist_lck"
-  if [ $? -eq 0 ]; then
-    "$ORACLE_BASE/$LOCKING_SCRIPT" --check --file "$ORACLE_BASE/oradata/${ORACLE_SID}.create_lck"
-    if [ $? -ne 0 ]; then
+"$ORACLE_BASE/$LOCKING_SCRIPT" --check --file "$ORACLE_BASE/oradata/${ORACLE_SID}.create_lck"
+if [ $? -ne 0 ]; then  # Ok create lock not held, now check status
+  "$ORACLE_BASE/$CHECK_DB_FILE"
+  if [ $? -ne 0 ]; then # DB status is not good
+    "$ORACLE_BASE/$LOCKING_SCRIPT" --check --file "$ORACLE_BASE/oradata/${ORACLE_SID}.exist_lck"
+    if [ $? -eq 0 ]; then # if exist lock is held release it
       "$ORACLE_BASE/$LOCKING_SCRIPT" --release --file "$ORACLE_BASE/oradata/${ORACLE_SID}.exist_lck"
+    else
+      exit 1
     fi
-  else
-    exit 1
   fi
 fi

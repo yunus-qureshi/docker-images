52c1d8b1b3c2614a4fef666e251693354be4fdf8
Mohammed Yunus <mohammed.qureshi@oracle.com>
2020-03-24 19:31:34 +0530

    Further enhance healt check
    Signed-off-by: Mohammed Yunus <mohammed.qureshi@oracle.com>

--- OracleDatabase/SingleInstance/extensions/fastfailover/checkDBLockStatus.sh
+++ OracleDatabase/SingleInstance/extensions/fastfailover/checkDBLockStatus.sh
@@ -14,14 +14,13 @@ ORACLE_SID="`grep $ORACLE_HOME /etc/oratab | cut -d: -f1`"
 if "$ORACLE_BASE/$LOCKING_SCRIPT" --check --file "$ORACLE_BASE/oradata/${ORACLE_SID}.create_lck"; then
   exit 1  # create lock held, DB is still initializing
 elif ! "$ORACLE_BASE/$CHECK_DB_FILE"; then
-  # DB status is not good, check if exist lock is held
-  if "$ORACLE_BASE/$LOCKING_SCRIPT" --check --file "$ORACLE_BASE/oradata/${ORACLE_SID}.exist_lck"; then
-    # exist lock is held release it
-    "$ORACLE_BASE/$LOCKING_SCRIPT" --release --file "$ORACLE_BASE/oradata/${ORACLE_SID}.exist_lck"
-  elif pgrep -f "$LOCKING_SCRIPT.*--acquire.*create_lck"; then
-    # blocked on create lock
+  # DB status is not good, check if blocked acquiring create lock
+  if pgrep -f "$LOCKING_SCRIPT.*--acquire.*create_lck"; then
+    # blocked acquiring create lock
     exit 1
   else
+    # if not blocked, release exist lock
+    "$ORACLE_BASE/$LOCKING_SCRIPT" --release --file "$ORACLE_BASE/oradata/${ORACLE_SID}.exist_lck"
     # Kill the process that keeps the container alive
     pkill -9 -f "tail.*alert"
   fi

a10491f95154f89cf63564394e57c637b369912d
Rishabh Gupta <rishabh.y.gupta@oracle.com>
2020-03-26 09:33:53 +0530

    added extension patching
    Signed-off-by: Rishabh Gupta <rishabh.y.gupta@oracle.com>

---
+++ OracleDatabase/SingleInstance/extensions/patching/patchDBBinaries.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# LICENSE UPL 1.0
+#
+# Copyright (c) 1982-2020 Oracle and/or its affiliates. All rights reserved.
+# 
+# Since: March, 2020
+# Author: rishabh.y.gupta@oracle.com
+# Description: Applies the patches provided by the user on the oracle home.
+# 
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+# 
---
+++ OracleDatabase/SingleInstance/extensions/patching/runOracle.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# LICENSE UPL 1.0
+#
+# Copyright (c) 1982-2020 Oracle and/or its affiliates. All rights reserved.
+#
+# Since: March, 2020
+# Author: rishabh.y.gupta@oracle.com
+# Description: Runs datapatch while migrating to a container with higher RU while using existing datafiles
+# 
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+#
+

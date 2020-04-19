6ec8b7d478c445cf21e4f55d8a21a561e824d526
Rishabh Gupta <rishabh.y.gupta@oracle.com>
2020-04-20 03:56:55 +0530

    updated patchDBBinaries.sh to apply RU as well as one-offs on Oracle Home
    Signed-off-by: Rishabh Gupta <rishabh.y.gupta@oracle.com>

--- OracleDatabase/SingleInstance/extensions/patching/patchDBBinaries.sh
+++ OracleDatabase/SingleInstance/extensions/patching/patchDBBinaries.sh
@@ -10,31 +10,53 @@
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 #
 
-PATCHES_LIST=()
+RU_DIR="${PATCH_DIR}/release_update"
+ONE_OFFS_DIR="${PATCH_DIR}/one_offs"
 
-if ls $PATCH_DIR/*.zip 2> /dev/null; then
-    for patch_zip in $PATCH_DIR/*.zip; do
+ru_count=$(ls $RU_DIR/*.zip 2> /dev/null | wc -l)
+if [ $ru_count -ge 2 ]; then
+    echo "Error: Only 1 Release Update can be applied."
+    exit 1;
+elif [ $ru_count == 1 ]; then
+    ru_patch="$(ls $RU_DIR/*.zip)"
+    echo "Unzipping $ru_patch";
+    unzip -qo $ru_patch -d $PATCH_DIR;
+    ru_patch=$(echo ${ru_patch##*/} | cut -d_ -f1 | cut -dp -f2)
+else
+    echo "No Release Update to be installed."
+fi
+
+ONE_OFFS_LIST=()
+
+if ls $ONE_OFFS_DIR/*.zip 2> /dev/null; then
+    for patch_zip in $ONE_OFFS_DIR/*.zip; do
         patch_no=$(echo ${patch_zip##*/} | cut -d_ -f1 | cut -dp -f2)
         if [ $patch_no == "6880880" ]; then
             echo "Removing directory ${ORACLE_HOME}/OPatch";
             rm -rf ${ORACLE_HOME}/OPatch;
             echo "Unzipping OPatch archive $patch_zip to ${ORACLE_HOME}";
-            unzip $patch_zip -d $ORACLE_HOME;
+            unzip -qo $patch_zip -d $ORACLE_HOME;
         else
-            PATCHES_LIST+=($patch_no);
+            ONE_OFFS_LIST+=($patch_no);
             echo "Unzipping $patch_zip";
-            unzip $patch_zip -d $PATCH_DIR;
+            unzip -qo $patch_zip -d $PATCH_DIR;
         fi
     done
 else
-    echo "No patches to be installed."
-    exit;
+    echo "No one-offs to be installed."
 fi
 
 export PATH=${ORACLE_HOME}/perl/bin:$PATH;
 
-for patch in ${PATCHES_LIST[@]}; do
-    echo "Applying patch $patch";
+if [ ! -z $ru_patch ]; then
+    echo "Applying Release Update: $ru_patch";
+    cmd="${ORACLE_HOME}/OPatch/opatchauto apply -binary -oh $ORACLE_HOME ${PATCH_DIR}/${ru_patch} -target_type rac_database";
+    echo "Running: $cmd";
+    $cmd;
+fi
+
+for patch in ${ONE_OFFS_LIST[@]}; do
+    echo "Applying patch: $patch";
     cmd="${ORACLE_HOME}/OPatch/opatchauto apply -binary -oh $ORACLE_HOME ${PATCH_DIR}/${patch} -target_type rac_database";
     echo "Running: $cmd";
     $cmd;

aeb87761c59164c1d83d118dbe9dd67b789f91d1
abhisbyk <abhishek.by.kumar@oracle.com>
2021-06-15 20:17:31 +0530

    Minor correction in checkDBStatus.sh
    Signed-off-by: abhisbyk <abhishek.by.kumar@oracle.com>

--- OracleDatabase/SingleInstance/dockerfiles/19.3.0/checkDBStatus.sh
+++ OracleDatabase/SingleInstance/dockerfiles/19.3.0/checkDBStatus.sh
@@ -17,7 +17,7 @@
 # Function to check database role: either Primary or Secondary
 checkDatabaseRole() {
    # Obtain DB_ROLE using SQLPlus
-   DB_ROLE=`sqlplus -s << EOF
+   DB_ROLE=`sqlplus -s / << EOF
 set heading off;
 set pagesize 0;
 SELECT database_role FROM v\\$database ;
@@ -38,7 +38,7 @@ EOF
 # Or in case of Secondary Database PDBs should be opened only in "READ ONLY" mode 
 checkPDBOpen() {
    # Obtain OPEN_MODE for PDB using SQLPlus
-   PDB_OPEN_MODE=`sqlplus -s << EOF
+   PDB_OPEN_MODE=`sqlplus -s / << EOF
 set heading off;
 set pagesize 0;
 SELECT DISTINCT open_mode FROM v\\$pdbs;

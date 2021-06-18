0adaf975fdb49d4a3f91214662e10c49ef7e0e70
psaini <psaini@slc17rku.us.oracle.com>
2021-06-18 16:52:16 -0700

    Added files to hanlde shutdown and startup using SID docker image files

--- OracleDatabase/SingleInstance/extensions/sharding/runOracle.sh
+++ OracleDatabase/SingleInstance/extensions/sharding/runOracle.sh
@@ -10,13 +10,48 @@
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 #
 
+########### SIGINT handler ############
+function _int() {
+   echo "Stopping container."
+   echo "SIGINT received, shutting down database!"
+   sqlplus / as sysdba <<EOF
+   shutdown immediate;
+   exit;
+EOF
+   lsnrctl stop
+}
+
+########### SIGTERM handler ############
+function _term() {
+   echo "Stopping container."
+   echo "SIGTERM received, shutting down database!"
+   sqlplus / as sysdba <<EOF
+   shutdown immediate;
+   exit;
+EOF
+   lsnrctl stop
+}
+
+########### SIGKILL handler ############
+function _kill() {
+   echo "SIGKILL received, shutting down database!"
+   sqlplus / as sysdba <<EOF
+   shutdown abort;
+   exit;
+EOF
+   lsnrctl stop
+}
+
+
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #
 ############# MAIN ################
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #
 ###################################
 
-if [ ${SHARD_SETUP,,} == "true" ]; then
- sh $ORACLE_BASE/scripts/sharding/runOraShardSetup.sh
+if [ ! -z ${SHARD_SETUP} ]; then
+ if [ ${SHARD_SETUP,,} == "true" ]; then
+   sh $ORACLE_BASE/scripts/sharding/runOraShardSetup.sh
+ fi
 fi
 
 if [ ! -z ${CLONE_DB} ]; then
@@ -26,12 +61,22 @@ if [ ! -z ${CLONE_DB} ]; then
  fi
 fi
 
-if [ ! -z ${OP_TYPE} ]; then  
+if [ ! -z ${OP_TYPE} ]; then
  if [ ${OP_TYPE,,} == "standbyshard" ]; then
    echo "The following output is now a tail of the alert.log:"
-   tail -f $ORACLE_BASE/diag/rdbms/*/*/trace/alert*.log &  
+   tail -f $ORACLE_BASE/diag/rdbms/*/*/trace/alert*.log &
  fi
 fi
+
+# Set SIGINT handler
+trap _int SIGINT
+
+# Set SIGTERM handler
+trap _term SIGTERM
+
+# Set SIGKILL handler
+trap _kill SIGKILL
+
 
 childPID=$!
 wait $childPID
